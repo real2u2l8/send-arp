a26a563f325bb23f503d91223a0ea2ded85e5438
real2u2l8 <real2u2l8@gmail.com>
2022-08-15 22:17:17 +0900

    del descriptions

--- main.cpp
+++ main.cpp
@@ -152,29 +152,4 @@ int main(int argc, char* argv[]) {
 	
 	sender_mac = getSenderMac(dev, handle, sender_ip); //sender(victime)의 mac주소를 알기위한 arp request
 	startInfection(dev, handle, sender_ip, sender_mac, target_ip);
-
-
-
-//	EthArpPacket packet; //이더넷, arp 패킷 구조를 선언 
-//	/*arp request 패킷 구조 만듥기*/
-//	packet.eth_.dmac_ = Mac("00:00:00:00:00:00");
-//	packet.eth_.smac_ = Mac("00:00:00:00:00:00");
-//	packet.eth_.type_ = htons(EthHdr::Arp);
-//
-//	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
-//	packet.arp_.pro_ = htons(EthHdr::Ip4);
-//	packet.arp_.hln_ = Mac::SIZE;
-//	packet.arp_.pln_ = Ip::SIZE;
-//	packet.arp_.op_ = htons(ArpHdr::Request);
-//	packet.arp_.smac_ = Mac("00:00:00:00:00:00");
-//	packet.arp_.sip_ = htonl(Ip("0.0.0.0"));
-//	packet.arp_.tmac_ = Mac("00:00:00:00:00:00");
-//	packet.arp_.tip_ = htonl(Ip("0.0.0.0"));
-//	/*패킷 sending*/
-//	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
-//	if (res != 0) {
-//		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
-//	}
-//
-//	pcap_close(handle);
 }
